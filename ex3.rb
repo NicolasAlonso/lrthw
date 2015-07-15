64b6674c8fcdd4aa5400337c61f5554ff3f2609c
Nicolas Alonso <alonso.nicolas@neuf.fr>
2015-07-15 22:51:58 +0200

    small changes in differnets exercices from 2 to 5

--- ex3.rb
+++ ex3.rb
@@ -1,7 +1,8 @@
 puts "I will now count my chickens:"
-
-puts "Hens #{25 + 30 / 6}"
-puts "Roosters #{100 - 25 * 3 % 4}"
+# Now it's time to do mathematics
+puts "Hens #{25.00 + 30.00 / 6.00}"
+#But what exactly does the octothorpe
+puts "Roosters #{100.00 - 25.00 * 3.00 % 4.00}"
 # 
 puts "Now I will count the eggs:"
 
@@ -17,8 +18,9 @@ puts "What is 5 - 7? #{5 - 7}"
 puts "Oh, that's why it's false."
 
 puts "How about some more."
-puts 5 + 8 + 2 - 5 % 10 / 1 + 1
-puts "Is it greater #{5 > -2}"
 
+puts 5 + 8 + 2 - 5 / 1 + 1
+puts "Is it greater? #{5 > -2}"
 puts "Is it greater or equal? #{5 >= -2}"
 puts "Is it less or equal? #{5 <= -2}"
+puts 3.0 / 2
--- ex4.rb
+++ ex4.rb
@@ -6,9 +6,9 @@ cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
-
+# Above is a list of different variable
 puts "In this case there is no difference with 4.0 or just 4"
-
+#There is no difference between 4 and 4.0
 puts "There are #{cars_driven} cars available."
 puts "There are only #{drivers} drivers available."
 puts "There will be #{cars_not_driven} empty cars today."
